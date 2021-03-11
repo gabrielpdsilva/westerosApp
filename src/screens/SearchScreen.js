826dc5e05fd7745bc0aab173493d19c70b052776
Gabriel Pimentel <gabriel.pimentel.sotc@gmail.com>
2021-03-11 12:39:30 -0300

    Fixing Search Screen, adding StackMain

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
-  StyleSheet,
-  TextInput,
+  StyleSheet
 } from 'react-native';
 
-const SearchScreen = () => {
+import {Picker} from '@react-native-picker/picker';
+
+const SearchScreen = ({navigation}) => {
+
+    const [selectedRegion, setSelectedRegion] = useState();
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Welcome!</Text>
-            <Text style={styles.text}>Type a region to list all Westeros' Houses.</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Type here..."
-                placeholderTextColor="#828282"
-            />  
+            <Text style={styles.text}>Select a region to list all Westeros' Houses from the chosen location.</Text>
+            <Picker
+                style={styles.picker}
+                selectedValue={selectedRegion}
+                onValueChange={(itemValue, itemIndex) => {
+                    setSelectedRegion(itemValue);
+                    navigation.push('ResultScreen', itemValue);
+                }
+            }>
+                <Picker.Item label="North" value="north" />
+                <Picker.Item label="Iron Islands" value="iron" />
+                <Picker.Item label="Riverlands" value="riverlands" />
+                <Picker.Item label="Vale" value="vale" />
+                <Picker.Item label="Westerlands" value="westerlands" />
+                <Picker.Item label="Stormlands" value="stormlands" />
+                <Picker.Item label="Reach" value="reach" />
+                <Picker.Item label="Crownlands" value="crownlands" />
+                <Picker.Item label="Dorne" value="dorne" />
+            </Picker>
         </View>
     )
 }
@@ -24,6 +41,11 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#0D1521',
+    },
+    picker: {
+        backgroundColor: '#060A11',
+        margin: 15,
+        color: '#828282'
     },
     title: {
         fontSize: 30,
@@ -35,11 +57,6 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: 'white',
         textAlign: 'center'
-    },
-    input: {
-        margin: 15,
-        backgroundColor: '#060A11',
-        color: 'white',
     },
     
 });
