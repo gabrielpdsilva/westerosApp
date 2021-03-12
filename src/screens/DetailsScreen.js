94887bbae9a6c2d9e13037f987beaa638b872dfa
Gabriel Pimentel <gabriel.pimentel.sotc@gmail.com>
2021-03-12 13:41:50 -0300

    Horizontal view has been customized

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -8,39 +8,47 @@ import {
 const DetailsScreen = () => {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>GENERAL INFORMATION</Text>
-            
-            <View style={{flexDirection: 'row'}}>
-                <Text style={styles.subtitle}>NAME</Text>
-                <Text style={styles.text}>Name example</Text>
-            </View>
-            
-            <View style={{flexDirection: 'row'}}>
-                <Text style={styles.subtitle}>REGION</Text>
-                <Text style={styles.text}>Region example</Text>
-            </View>
 
-            <View style={{flexDirection: 'row'}}>
-                <Text style={styles.subtitle}>CURRENT LORD</Text>
-                <Text style={styles.text}>Current lord example</Text>
+            <View style={{margin: 10}}>
+
+                <Text style={styles.title}>GENERAL INFORMATION</Text>
+                
+                <View style={styles.horizontalView}>
+                    <Text style={styles.subtitle}>NAME</Text>
+                    <Text style={styles.text}>Name example</Text>
+                </View>
+                <View style={styles.horizontalView}>
+                    <Text style={styles.subtitle}>REGION</Text>
+                    <Text style={styles.text}>Region example</Text>
+                </View>
+                <View style={styles.horizontalView}>
+                    <Text style={styles.subtitle}>CURRENT LORD</Text>
+                    <Text style={styles.text}>Current lord example</Text>
+                </View>
+
             </View>
 
             <View style={styles.line}/>
 
-            <Text style={styles.title}>MORE DETAILS</Text>
+            <View  style={{margin: 10}}>
 
-            <View style={{flexDirection: 'row'}}>           
-                <Text style={styles.subtitle}>FOUNDER</Text>
-                <Text style={styles.text}>Founder example</Text>
+                <Text style={styles.title}>MORE DETAILS</Text>
+
+                <View style={styles.horizontalView}>           
+                    <Text style={styles.subtitle}>FOUNDER</Text>
+                    <Text style={styles.text}>Founder example</Text>
+                </View>
+                <View style={styles.horizontalView}>
+                    <Text style={styles.subtitle}>WORDS</Text>
+                    <Text style={styles.text}>Words example</Text>
+                </View>
+                <View style={styles.horizontalView}>
+                    <Text style={styles.subtitle}>COAT OF ARMS</Text>
+                    <Text style={styles.text}>Coat of arms example</Text>
+                </View>
+
             </View>
-            <View style={{flexDirection: 'row'}}>
-                <Text style={styles.subtitle}>WORDS</Text>
-                <Text style={styles.text}>Words example</Text>
-            </View>
-            <View style={{flexDirection: 'row'}}>
-                <Text style={styles.subtitle}>COAT OF ARMS</Text>
-                <Text style={styles.text}>Coat of arms example</Text>
-            </View>
+
         </View>
     )
 }
@@ -58,7 +66,8 @@ const styles = StyleSheet.create({
     subtitle: {
         fontSize: 15,
         color: '#FF773A',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginRight: 50
     },
     text: {
         fontSize: 15,
@@ -67,7 +76,13 @@ const styles = StyleSheet.create({
     line: {
         marginTop: 5,
         borderWidth: 0.5,
-        borderColor:'#5e2f0e',
+        borderColor:'#5e2d0a',
+    },
+    horizontalView: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginLeft: 20,
+        marginRight: 20
     }
 });
 
